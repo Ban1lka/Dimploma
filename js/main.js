23ef79463df1325566bd0ce29acaa109eb7c1d44
Yaroslav Veletnyuk <vacoous@gmail.com>
2025-05-09 14:57:13 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -270,7 +270,7 @@ console.table(Object.keys(localStorage).filter(key => key.includes('_email')).ma
     const email = key.replace('_email', '');
     return {
         Email: localStorage.getItem(key),
-         Password: typeof password === 'string' ? '*'.repeat(password.length) : '(no password)'
+        
     };
 }));
 
